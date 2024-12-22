6fbb3e3fdc1c736f3c08e13a07e6c876a80e1fc7
ibrahimabdulmumini <www.ibrahimabdulmumini@gmail.com>
2024-12-22 16:13:11 +0700

    Update HashSetExample1.java
    112

--- 19JSP02/src/jsp/examples/collections/HashSetExample1.java
+++ 19JSP02/src/jsp/examples/collections/HashSetExample1.java
@@ -18,7 +18,7 @@ public class HashSetExample1 {
         // Add objects to hashset
         set.add("Java222");
         set.add("C++");
-        set.add("PHP");
+        set.add("PHP111");
         set.add("Java11");
         // Show set through Iterator
         Iterator<String> itr = set.iterator();
