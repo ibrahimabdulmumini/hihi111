643346f7e545a931f19fccbafac221b3ddeec640
ibrahimabdulmumini <www.ibrahimabdulmumini@gmail.com>
2025-01-01 07:49:37 +0700

    Update CollectionExample1.java
    fds213

--- 19JSP02/src/jsp/examples/collections/CollectionExample1.java
+++ 19JSP02/src/jsp/examples/collections/CollectionExample1.java
@@ -18,7 +18,7 @@ import java.util.Set;
 public class CollectionExample1 {
 	public static void main(String[] args) {
         List<String> arrayList = new ArrayList<String>();
-        arrayList.add("PHP1");
+        arrayList.add("P123HP1");
         arrayList.add("Python1");
         arrayList.add("Java1");
         arrayList.a134dd("C++");
