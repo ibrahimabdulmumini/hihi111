6086eccbeba49ee7de6d06495544e95fc37bbf8d
ibrahimabdulmumini <www.ibrahimabdulmumini@gmail.com>
2025-01-16 14:05:54 +0700

    Update ArrayListExample1.java
    fd2

--- 19JSP02/src/jsp/examples/collections/ArrayListExample1.java
+++ 19JSP02/src/jsp/examples/collections/ArrayListExample1.java
@@ -15,7 +15,7 @@ public class ArrayListExample1 {
         // Creating arraylist
         ArrayList<String> list = new ArrayList<String>();
         // Add objects to arraylist
-        list.add("Java"213);
+        list.add("Java");
         list.add("C++");
         list.add("PHP");
         list.add("Java");
